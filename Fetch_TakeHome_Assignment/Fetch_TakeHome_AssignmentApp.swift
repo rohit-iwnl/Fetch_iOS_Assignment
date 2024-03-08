7cdf6fca359f98aec19d1bd404ab9936fd6d9867
rohit-iwnl <rohitmanivel9@gmail.com>
2024-03-07 19:57:12 -0700

    Initial Commit

---
+++ Fetch_TakeHome_Assignment/Fetch_TakeHome_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_TakeHome_AssignmentApp.swift
+//  Fetch_TakeHome_Assignment
+//
+//  Created by Rohit Manivel on 3/7/24.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_TakeHome_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
